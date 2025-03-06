79e885031cc6236ac8f088c597d57d263d3cc917
JingChen-Thu <cjcjyu597460474@163.com>
2025-03-06 11:56:47 +0800

    check kernel density error

--- include/smooth.h
+++ include/smooth.h
@@ -239,7 +239,15 @@ inline void smooth_inv_kernels_orig(Grid& grid, InputParams& IP) {
                     // do nothing
                 } else {
                     if (grid.Ks_density_loc[I2V(i_loc,j_loc,k_loc)] < 0){
-                        std::cout << "Warning: grid.Ks_density_loc[I2V(" << i_loc << "," << j_loc << "," << k_loc << ")] is less than 0, = " << grid.Ks_density_loc[I2V(i_loc,j_loc,k_loc)] << std::endl;
+                        std::cout   << "Warning: grid.Ks_density_loc[I2V(" << i_loc << "," << j_loc << "," << k_loc << ")] is less than 0, = " 
+                                    << grid.Ks_density_loc[I2V(i_loc,j_loc,k_loc)]                           
+                                    << std::endl;
+                        // print the source name
+                        for (int i_src = 0; i_src < IP.n_src_comm_rec_this_sim_group; i_src++){
+                            // get source info
+                            std::string name_sim_src   = IP.get_src_name_comm(i_src);
+                            std::cout << "id_sim: " << id_sim+1 << "/" << IP.n_src_comm_rec_this_sim_group << ", source name: " << name_sim_src << std::endl;
+                        }
                     }
                     grid.Ks_loc[I2V(i_loc,j_loc,k_loc)] /= std::pow(std::abs(grid.Ks_density_loc[I2V(i_loc,j_loc,k_loc)]),Kdensity_coe);
                 }
