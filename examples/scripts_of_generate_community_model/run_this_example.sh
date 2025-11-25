fd92b5321d22880ff9e4e0c54d939aed16e51348
JingChen-Thu <cjcjyu597460474@163.com>
2025-11-25 21:15:45 +0800

    add citations

--- examples/scripts_of_generate_community_model/run_this_example.sh
+++ examples/scripts_of_generate_community_model/run_this_example.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # run the script to generate HDF5 model files based one community models
-# 1. crust1.0 + ak135 model
+# 1. crust1.0 (Laske et al., 2013) + ak135 model (Kennett et al., 1995)
 python 1_crust1.0_ak135_model.py
 
 
+# References:
+# Laske, G., Masters, G., Ma, Z., & Pasyanos, M. (2013, April). Update on CRUST1. 0—A 1-degree global model of Earth’s crust. In Geophysical research abstracts (Vol. 15, No. 15, p. 2658).
+# Kennett, B. L., Engdahl, E. R., & Buland, R. (1995). Constraints on seismic velocities in the Earth from traveltimes. Geophysical Journal International, 122(1), 108-124.
