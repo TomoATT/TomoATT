109a22aeac804f9ee47ce5d4b4455a0e8b91fa69
JingChen-Thu <cjcjyu597460474@163.com>
2025-12-11 16:55:43 +0800

    eg1 can work with gd or bfgs with or without line search

--- src/optimizer.cpp
+++ src/optimizer.cpp
@@ -189,8 +189,6 @@ void Optimizer::determine_step_length_controlled(InputParams& IP, Grid& grid, in
 
 
 // determine step length (line search method)
-// 用二次函数近似的方式，最多给1次试探步。
-// 设置最大步长
 std::vector<CUSTOMREAL> Optimizer::determine_step_length_line_search(InputParams& IP, Grid& grid, IO_utils& io, int i_inv, CUSTOMREAL& v_obj_inout) {
     
     std::vector<CUSTOMREAL> v_obj_misfit_line_search;
@@ -208,19 +206,18 @@ std::vector<CUSTOMREAL> Optimizer::determine_step_length_line_search(InputParams
 
     // ----------------------- step 2, do line search -----------------------
     CUSTOMREAL alpha = step_length_init;        // initial step length
-    int quit_sub_iter = 2;                      // maximum sub-iteration number to quit (avoid too many sub-iterations)
-    CUSTOMREAL alpha_R = _0_CR;                 // upper bound of step length
-    CUSTOMREAL alpha_L = _0_CR;                 // lower bound of step length
-    std::vector<CUSTOMREAL> alpha_sub_iter(quit_sub_iter+1);     // store tried step lengths
-    std::vector<CUSTOMREAL> v_obj_sub_iter(quit_sub_iter+1);     // store objective function values at tried step lengths
-
-
+    // int quit_sub_iter = 2;                      // maximum sub-iteration number to quit (avoid too many sub-iterations)
+    // CUSTOMREAL alpha_R = _0_CR;                 // upper bound of step length
+    // CUSTOMREAL alpha_L = _0_CR;                 // lower bound of step length
+    std::vector<CUSTOMREAL> alpha_sub_iter(3);     // store tried step lengths
+    std::vector<CUSTOMREAL> v_obj_sub_iter(3);     // store objective function values at tried step lengths
     // constant value for curvature condition (p_k is descent direction, Ks_update_loc is ascent direction, so use negative value)
-    CUSTOMREAL c2_inner_product_old = - 0.9 * grid_value_dot_product(grid.Ks_update_loc, grid.Ks_loc, n_total_loc_grid_points);
-    CUSTOMREAL angle_old = calculate_angle_between_grid_values(grid.Ks_update_loc, grid.Ks_loc, n_total_loc_grid_points);
+    // CUSTOMREAL c2_inner_product_old = - 0.9 * grid_value_dot_product(grid.Ks_update_loc, grid.Ks_loc, n_total_loc_grid_points);
+    // CUSTOMREAL angle_old = calculate_angle_between_grid_values(grid.Ks_update_loc, grid.Ks_loc, n_total_loc_grid_points);
 
+    bool exit_flag = false;
     // main line search iteration
-    for(int sub_iter = 0; sub_iter < 100; sub_iter++){
+    for(int sub_iter = 0; sub_iter <= 2; sub_iter++){
 
         if (myrank == 0 && id_sim == 0){
             std::cout << "Line search sub-iteration " << sub_iter << ", try step length alpha = " << alpha << std::endl << std::endl;
@@ -244,104 +241,142 @@ std::vector<CUSTOMREAL> Optimizer::determine_step_length_line_search(InputParams
         CUSTOMREAL v_obj_try = v_obj_misfit_line_search[0];
 
         // substep 4, --------- evaluate the update performance ---------     
-          
-        bool cond_armijo    = check_armijo_condition(v_obj_inout, v_obj_try);      // condition 1, Armijo condition
-        // bool cond_curvature = check_curvature_condition(grid, c2_inner_product_old); // condition 2, curvature condition
-        bool cond_angle     = false;
-        CUSTOMREAL angle_new    = calculate_angle_between_grid_values(grid.Ks_update_loc, grid.Ks_loc, n_total_loc_grid_points);    // have broadcasted within one subdomain
-        if (std::abs(angle_new - angle_old) > 20.0 &&  std::abs(angle_new - angle_old) < 160.0){
-            cond_angle = true;
-        }
+        
+        // There are 8 ways to adjust step:
+        // The current model: (step, obj) = (0, v_obj_inout)
+        // The first try: (alpha, v1)
+        // If v1 < v_obj_inout. Then, the second try: (2*alpha, v2), else (alpha/2, v2). So, the 6 cases are: 
+        // v1 > v_obj_inout, (0, alpha, 1/2 alpha), quadratic: y = a*x^2 + b*x + v_obj_inout, optimal step alpha_quad = -b/2a
+        // 1. a < 0, third try: 1/4 alpha                       (a new step, need to do forward modeling)
+        // 2. a > 0, alpha_quad < 0, third try: 1/4 alpha       (a new step)
+        // 3. a > 0, alpha_quad > 0, third try, alpha_quad      (a new step) if alpha_quad < 1/4 alpha, using 1/4 alpha to avoid too small step
+        // v1 < v_obj_inout, (0, alpha, 2*alpha), 
+        // 4. a < 0, third try: 2*alpha                         (have done, no need to do forward modeling)
+        // 5. a > 0, alpha_quad > 2*alpha, third try: 2*alpha   (have done) Now, alpha_quad mush be greater than 0, so, just select 2*alpha
+        // 6. a > 0, alpha_quad < 2*alpha, third try: alpha_quad (a new step) if alpha_quad < alpha/2, using alpha/2 to avoid too small step
 
-        // check conditions
-        if(myrank == 0 && id_sim == 0){
-            std::cout << std::endl; 
-            std::cout << "Evaluate conditions at sub-iteration " << sub_iter << ": " << std::endl;
-            std::cout << "    Tried step length alpha = " << alpha << std::endl;
-            std::cout << "    Objective function value at current model: " << v_obj_inout << std::endl;
-            std::cout << "    Objective function value at tried model: " << v_obj_try << std::endl;
-            std::cout << "    Armijo condition satisfied: " << std::boolalpha << cond_armijo << std::endl;
-            std::cout << "    Angle between descent direction and gradient at current model: " << angle_old << " degree" << std::endl;
-            std::cout << "    Angle between descent direction and gradient at tried model: " << angle_new << " degree" << std::endl;
-            std::cout << "    Angle condition satisfied (dif > 20 degree): " << std::boolalpha << cond_angle << std::endl;
-        }
-
-        alpha_sub_iter[sub_iter] = alpha;
-        v_obj_sub_iter[sub_iter] = v_obj_try;
-
-        // adjust step length
-        if(sub_iter == quit_sub_iter-1){
-            // must 
-        } else {
-
-        }
-
-
-        if (cond_armijo && cond_curvature){
-            // satisfy Wolfe conditions
+        if(sub_iter == 0){      // first try
+            alpha_sub_iter[0] = alpha;
+            v_obj_sub_iter[0] = v_obj_try;
             if(myrank == 0 && id_sim == 0){
-                std::cout << "Satisfy Wolfe conditions at sub-iteration " << sub_iter 
-                        << ", step length alpha = " << alpha 
-                        << ", obj = " << v_obj_try << std::endl;
+                std::cout << std::endl; 
+                std::cout << "Evaluate line search at sub-iteration 1: " << std::endl;
+                std::cout << "    Tried step length alpha = " << alpha << std::endl;
+                std::cout << "    Objective function value at current model: " << v_obj_inout << std::endl;
+                std::cout << "    Objective function value at tried model: " << v_obj_try << std::endl;
+            }
+            if (v_obj_try > v_obj_inout){        // objective function increases
+                alpha = 0.5 * alpha;
+                if(myrank == 0 && id_sim == 0){
+                    std::cout << "    Objective function increases, so step length decreases to: " << alpha << std::endl;
+                }
+            } else {                            // objective function decreases
+                alpha = 2.0 * alpha;    
+                if(myrank == 0 && id_sim == 0){
+                    std::cout << "    Objective function decreases, so step length increases to: " << alpha << std::endl;
+                }
+            }
+            if(myrank == 0 && id_sim == 0){
+                std::cout << std::endl;
+            }
+        } else if (sub_iter == 1){      // second try
+            alpha_sub_iter[1] = alpha;
+            v_obj_sub_iter[1] = v_obj_try;
+            CUSTOMREAL x1 = alpha_sub_iter[0];
+            CUSTOMREAL x2 = alpha_sub_iter[1];
+            CUSTOMREAL y0 = v_obj_inout;
+            CUSTOMREAL y1 = v_obj_sub_iter[0];
+            CUSTOMREAL y2 = v_obj_sub_iter[1];
+            CUSTOMREAL quad_a = ((y2 - y0)*x1 - (y1 - y0)*x2) / (x1*x2*(x2-x1));
+            CUSTOMREAL quad_b = ((y2 - y0)*x1*x1 - (y1 - y0)*x2*x2) / (x1*x2*(x1-x2));
+            CUSTOMREAL alpha_quad = - quad_b / (2.0 * quad_a);
+            if(myrank == 0 && id_sim == 0){
+                std::cout << std::endl; 
+                std::cout << "Evaluate line search at sub-iteration 2: " << std::endl;
+                std::cout << "    Info of three models: " << std::endl;
+                std::cout << "    Step lengths   (x): " << 0 << ", " << x1 << ", " << x2 << std::endl;
+                std::cout << "    Obj fun values (y): " << y0 << ", " << y1 << ", " << y2 << std::endl;
+                std::cout << "    Quadratic regression: y = " << quad_a << " * x^2 + " << quad_b << " * x + " << y0 << std::endl;
+            }
+            if(y1 > y0){       // path: 0, alpha, 1/2 alpha
+                if(quad_a <= 0){                // case 1, a < 0
+                    alpha = 0.5 * alpha;
+                    if(myrank == 0 && id_sim == 0){
+                        std::cout << "    Quadratic regression failed (a <= 0), a = " << quad_a << ", reduce step length to: " << alpha << std::endl;
+                    }
+                } else if (alpha_quad <= 0){    // case 2, a > 0, alpha_quad < 0
+                    alpha = 0.5 * alpha;
+                    if(myrank == 0 && id_sim == 0){
+                        std::cout << "    Quadratic regression gives negative step length, alpha_quad = " << alpha_quad << ", reduce step length to: " << alpha << std::endl;
+                    }
+                } else {    // case 3, a > 0, alpha_quad > 0
+                    if (alpha_quad > 0.5 * alpha) {
+                        alpha = alpha_quad;
+                        if(myrank == 0 && id_sim == 0){
+                            std::cout << "    Quadratic regression gives positive step length, alpha_quad = " << alpha_quad << ", accept this step length." << std::endl;
+                        }
+                    } else {
+                        alpha = 0.5 * alpha;
+                        if(myrank == 0 && id_sim == 0){
+                            std::cout << "    Quadratic regression gives too small step length, alpha_quad = " << alpha_quad << ", reduce step length by half to: " << alpha << std::endl;
+                        }
+                    }
+                }
+            } else {        // path: 0, alpha, 2*alpha
+                if(quad_a <= 0){        // case 4, a < 0
+                    exit_flag = true;
+                    if(myrank == 0 && id_sim == 0){
+                        std::cout << "    Quadratic regression failed (a <= 0), a = " << quad_a << ", choose the second try, the step length is: " << alpha << std::endl;
+                    }
+                } else if (alpha_quad >= alpha){    // case 5, a > 0, alpha_quad > 2*alpha
+                    exit_flag = true;
+                    if(myrank == 0 && id_sim == 0){
+                        std::cout << "    Quadratic regression gives too large step length, alpha_quad = " << alpha_quad << ", choose the second try, the step length is: " << alpha << std::endl;
+                    }
+                } else {    // case 6, a > 0, alpha_quad < 2*alpha
+                    if (alpha_quad > 0.25 * alpha) {
+                        alpha = alpha_quad;
+                        if(myrank == 0 && id_sim == 0){
+                            std::cout << "    Quadratic regression gives moderate step length, alpha_quad = " << alpha_quad << ", accept this step length." << std::endl;
+                        }
+                    } else {
+                        alpha = 0.25 * alpha;
+                        if(myrank == 0 && id_sim == 0){
+                            std::cout << "    Quadratic regression gives too small step length, alpha_quad = " << alpha_quad << ", reduce step length by quarter to: " << alpha << std::endl;
+                        }
+                    }
+                }
+            }
+            if(myrank == 0 && id_sim == 0){
+                std::cout << std::endl;
+            }
+        } else {        // final try
+            exit_flag = true;
+            alpha_sub_iter[2] = alpha;
+            v_obj_sub_iter[2] = v_obj_try;
+            if(myrank == 0 && id_sim == 0){
+                std::cout << std::endl; 
+                std::cout << "    Tried step length alpha = " << alpha << std::endl;
+                std::cout << "    Objective function value at current model: " << v_obj_inout << std::endl;
+                std::cout << "    Objective function value at tried model: " << v_obj_try << std::endl;
+                std::cout << std::endl;
+            }
+        }
+
+
+        if(exit_flag){
+            if(myrank == 0 && id_sim == 0){
+                std::cout << std::endl; 
+                std::cout << "Line search ends. The search process contains  " << sub_iter+1 << " sub-iterations." << std::endl;
+                std::cout << "The information is summarized as follows: " << std::endl;
+                for(int tmp_i = 0; tmp_i <= sub_iter; tmp_i++){
+                    std::cout   << std::setw(25) << "    Sub-iter " << tmp_i+1 << ",";
+                    std::cout   << std::setw(25) << " step length = " << alpha_sub_iter[tmp_i];
+                    std::cout   << std::setw(25) << ", obj value = " << v_obj_sub_iter[tmp_i] << std::endl;
+                }
+                std::cout << std::endl;
             }
             break;
-        } else if (!cond_armijo && cond_curvature){
-            // only satisfy curvature condition, step length is too large
-            alpha_R = alpha;
-            alpha = (alpha_L + alpha_R) / 2.0;
-            if (sub_iter == quit_sub_iter) {
-                if (myrank == 0 && id_sim == 0){
-                    std::cout   << "Quit line search due to too many tries. Armijo condition not satisfied. "
-                                << "Reduce the initial step length to " << alpha << " at the next iteration." << std::endl;
-                }
-                break;
-            } else {
-                if (myrank == 0 && id_sim == 0){
-                    std::cout << "Armijo condition not satisfied" 
-                            << ", step length may be too large, Reduce the searching step length from " << alpha_R 
-                            << " to " << alpha << std::endl;
-                }
-            }
-            
-        } else if (cond_armijo && !cond_curvature){
-            // only satisfy Armijo condition, step length is too small
-            alpha_L =  alpha;
-            if(alpha_R != _0_CR){
-                alpha = (alpha_L + alpha_R) / 2.0;
-            } else {
-                alpha = 2.0 * alpha;
-            }
-            if (sub_iter == quit_sub_iter) {
-                if (myrank == 0 && id_sim == 0){
-                    std::cout << "Quit line search due to too many tries. Curvature condition not satisfied. "
-                                << "Increase the initial step length to " << alpha << " at the next iteration." << std::endl;
-                }
-                break;
-            } else {
-                if (myrank == 0 && id_sim == 0){
-                    mpi_debug_print_ranks();
-                    std::cout << "Curvature condition not satisfied" 
-                            << ", step length may be too small, Increase the searching step length from " << alpha_L 
-                            << " to " << alpha << std::endl;
-                }
-            }
-        } else {
-            // neither condition is satisfied
-            alpha_R = alpha;
-            alpha = (alpha_L + alpha_R) / 2.0;
-            if (sub_iter == quit_sub_iter) {
-                if (myrank == 0 && id_sim == 0){
-                    std::cout   << "Quit line search due to too many tries. Curvature and Armijo conditions not satisfied. "
-                                << "Reduce the initial step length to " << alpha << " at the next iteration." << std::endl;
-                }
-                break;
-            } else {
-                if (myrank == 0 && id_sim == 0){
-                    std::cout << "Curvature and Armijo conditions not satisfied" 
-                            << ", step length may be too large, Reduce the searching step length from " << alpha_R 
-                            << " to " << alpha << std::endl;
-                }
-            }
         }
 
     } // end for sub_iter
@@ -453,7 +488,6 @@ CUSTOMREAL Optimizer::calculate_angle_between_grid_values(CUSTOMREAL* vec1, CUST
         cos_angle = inner_product / (std::sqrt(norm_grad) * std::sqrt(norm_grad_previous));
         angle     = acos(cos_angle) * RAD2DEG;
     }
-    broadcast_cr_single_sub(angle, 0);  // broadcast within one subdomain
     return angle;
 }
 
